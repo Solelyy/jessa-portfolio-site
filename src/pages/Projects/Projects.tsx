8fabec37522313b90653ee8a93fda97a6eba9c5f
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2026-01-03 14:45:29 +0800

    modify margin

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -2,7 +2,7 @@ import ProjectGrid from "@/components/cards/ProjectGrid";
 
 export default function Projects () {
 return (
-    <main className="min-h-screen">
+    <main className="min-h-screen mb-40">
         <ProjectGrid />
     </main>
 );
