6d1a669977bc4811f7ad8ee87e9d758cefd23744
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2026-01-04 23:58:43 +0800

    Embed google map

--- src/components/cards/LocationCard.tsx
+++ src/components/cards/LocationCard.tsx
@@ -1,11 +1,21 @@
-import locationImage from "@/assets/images/location-image.png"
 export default function LocationCard() {
-    return (
-         <main className="bg-white dark:bg-darkCard border border-lightBorder dark:border-darkBorder rounded-2xl">
-            <img 
-            src={locationImage} 
-            alt="Location on the map"
-            className="rounded-2xl" />
-        </main>
-    );
+  return (
+    <div className="bg-white dark:bg-darkCard border border-lightBorder dark:border-darkBorder rounded-2xl p-6 space-y-4">
+
+    <p className="text-sm text-muted-foreground">
+        📍 Quezon City, Philippines
+    </p>
+
+      {/* Map */}
+      <div className="overflow-hidden rounded-xl border border-lightBorder dark:border-darkBorder">
+        <iframe
+          title="My Location"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15437.078384705352!2d121.02201159367463!3d14.697340228522197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b127a3ab5e71%3A0xf3288aa894218d9d!2sBagbag%2C%20Quezon%20City%2C%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1767534273360!5m2!1sen!2sph"
+          className="block w-full h-64"
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </div>
+    </div>
+  );
 }
